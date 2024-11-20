a635209ccd75791923b7075faef33a120109af26
Sharathxct <sharathxct@gmail.com>
2024-11-20 10:41:23 +0530

    RNR Framework

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from "react-native";
-import React from 'react'
+import { View, } from "react-native";
+import { Text } from '~/components/ui/text';
 
 const TabsLayout = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className='text-3xl'>Tabs</Text>
-    </View>
-  )
-}
+    <>
+      <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
+        <Text>Tabs Screen</Text>
+      </View>
+    </>
+  );
+};
 
 export default TabsLayout;
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,12 +1,29 @@
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
-import { verifyInstallation } from 'nativewind';
+import { router } from 'expo-router';
+import * as React from 'react';
+import { View } from 'react-native';
+import { Button } from '~/components/ui/button';
+import { Text } from '~/components/ui/text';
 
-export default function App() {
-  verifyInstallation();
+
+export default function Screen() {
+  const onPress = () => {
+    router.push('/(auth)/sign-in');
+  };
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className='text-3xl'>Open up!</Text>
+    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
+      <Text className='text-3xl'>Logo</Text>
+      <Text>Onboarding Screen</Text>
+      <Button
+        className='bg-primary text-primary w-full'
+        onPress={onPress}
+      >
+        <Text>Continue</Text>
+      </Button>
+      <Text className='text-sm pt-0 mt-[-3]' >
+        By continuing you accept to our terms and conditions.
+        <Text className='underline'>Terms and Conditions</Text>
+      </Text>
     </View>
   );
 }
